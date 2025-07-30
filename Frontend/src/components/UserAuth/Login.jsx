24afbe0035c84b8382034ef5fa336c9a805ca5ba
Vignan Baratam <119101138+vignanbartam@users.noreply.github.com>
2025-07-30 22:05:27 +0530

    Update Login.jsx

--- Frontend/src/components/UserAuth/Login.jsx
+++ Frontend/src/components/UserAuth/Login.jsx
@@ -11,7 +11,7 @@ function Login({ setIsLoggedIn, setUserName, setActiveCategory }) {
     setErrorMessage(""); // Clear previous errors
 
     try {
-      const response = await fetch("http://localhost:5050/api/auth/login", {
+      const response = await fetch("https://mern-shopping-zu9b.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ emailOrMobile, password }),
